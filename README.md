cdde3068ecaa6825d459294f2209a93b7263e8bd
jasonromulus <pumcome@me.com>
2018-12-07 10:16:20 -0800

    Made tiny changes

---
+++ README.md
@@ -0,0 +1,3 @@
+This an website where we can gossip about the entertainment world around. Sure there are already plenty of these type of websites but does it let you spill the tea is the better question. My code on this project is very basic as my knowledge is limitied. But please enjoy spilling tea when you look at my project.
+
+Jason Romulus
--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 // The stuff to start it
 const express = require('express');
 const app = express();
-const path = require('path');
-const exphbs = require('express-handlebars').create({
-    layoutsDir: path.join(__dirname, "views/layouts"),
-    partialsDir: path.join(__dirname, "views/partials"),
-    defaultLayout: 'main',
-    extname: 'hbs'
-  });
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+const exphbs = require('express-handlebars').create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+});
+
+//Mongoose Connections
 const mongoose = require('mongoose');
+
+//Handlebars stuff
+app.engine('hbs', exphbs.engine);
+app.set('view engine', 'hbs');
+
+// middleware 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+//connect mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/xoxo-gossip-people', { useNewUrlParser: true });
 
 //Controllers that are needed
 const news = require('./controllers/news.js');
 const articles = require('./controllers/articles.js');
 const notes = require('./controllers/notes.js');
-
-// middleware
-app.engine('hbs', exphbs.engine);
-app.set('view engine', 'hbs');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/xoxo-gossip-people', { useNewUrlParser: true });
 
 //these access my controls
 app.use(news);
